24bd86f4e62c7399df23547f74865722ea84c789
Jong1co <narira0922@gmail.com>
2023-02-05 13:48:21 +0900

    feature: add footer, banner, group section

---
+++ pages/Home/components/Footer/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as Style from './style';
+
+export const Footer = () => {
+  return <Style.Footer>개인정보 처리 방침</Style.Footer>;
+};
---
+++ pages/Home/components/GroupSection/style.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const GroupSection = styled.section`
+  width: 100%;
+  padding-bottom: 48px;
+  background-color: ${({ theme }) => theme.colors.neutral_200_b};
+`;
