104d9ad9d3dd3f6400141b43ae338027b5d740e1
Jong1co <narira0922@gmail.com>
2023-02-08 21:08:15 +0900

    style: home style 수정

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -1,15 +1,23 @@
-
+import React from 'react';
 import { Header, Banner, GroupSection, Footer } from './components';
+import Modal from '../../common/Modal';
+import { Layout } from './layouts';
 
 const Home = () => {
   return (
     /** layout 자리 */
-    <main>
+    <Layout>
       <Header />
       <Banner />
       <GroupSection />
       <Footer />
-    </main>
+      {/* <Modal.Frame isOpen={true} onClick={() => console.log('hi')}>
+        <Modal.Header>모임 만들기</Modal.Header>
+        <Modal.Body>
+          <input type="text" />
+        </Modal.Body>
+      </Modal.Frame> */}
+    </Layout>
   );
 };
 
