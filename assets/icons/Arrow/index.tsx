bfe5a1f2cf6d1476fdde4d8a00ae24b08db415d5
Jong1co <narira0922@gmail.com>
2023-02-15 20:05:24 +0900

    chore: add arrow_down icon size

--- assets/icons/Arrow/index.tsx
+++ assets/icons/Arrow/index.tsx
@@ -1,4 +1,3 @@
-
 import DoubleRight from './Doubleright.svg';
 import Down from './Down.svg';
 import Left from './Left.svg';
@@ -7,7 +6,8 @@ import Solid from './Solid.svg';
 
 export const ARROW = {
   DOUBLE_RIGHT: <DoubleRight />,
-  DOWN: <Down />,
+  DOWN_SM: <Down width={16} height={16} />,
+  DOWN_LG: <Down width={24} height={24} />,
   LEFT: <Left />,
   RIGHT: <Right />,
   SOLID: <Solid />,
