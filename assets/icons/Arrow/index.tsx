0e85106990502ed89341fe59a4691811858f8edf
Jong1co <narira0922@gmail.com>
2023-02-28 23:18:48 +0900

    chore: add double_left icon

--- assets/icons/Arrow/index.tsx
+++ assets/icons/Arrow/index.tsx
@@ -1,4 +1,5 @@
 import DoubleRight from './Doubleright.svg';
+import DoubleLeft from './DoubleLeft.svg';
 import Down from './Down.svg';
 import Left from './Left.svg';
 import Right from './Right.svg';
@@ -6,6 +7,7 @@ import Solid from './Solid.svg';
 
 export const ARROW = {
   DOUBLE_RIGHT: <DoubleRight />,
+  DOUBLE_LEFT: <DoubleLeft />,
   DOWN_SM: <Down width={16} height={16} />,
   DOWN_LG: <Down width={24} height={24} />,
   LEFT: <Left />,
