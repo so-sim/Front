9c336246faa42543ee759c448ccf9c25298b9167
Jong1co <narira0922@gmail.com>
2023-03-02 22:49:05 +0900

    feature: 이름 검사 정규식 수정

--- utils/validation.ts
+++ utils/validation.ts
@@ -1,5 +1,5 @@
 export const isValid = (text: string, min: number = 1, max: number = 15): boolean => {
   if (text.trim().length === 0) return false;
-  const regExp = new RegExp(`^[a-zA-Zㄱ-ㅎ가-힣]{${min},${max}}$`);
+  const regExp = new RegExp(`\.{${min},${max}}`);
   return regExp.test(text);
 };
