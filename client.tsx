8a727b8d720ca01717558847bd8a91039000538d
Jong1co <narira0922@gmail.com>
2023-03-05 14:30:29 +0900

    feature: msw설정

--- client.tsx
+++ client.tsx
@@ -2,6 +2,11 @@ import { createRoot } from 'react-dom/client';
 import React from 'react';
 import App from './App';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 const container = document.getElementById('app');
 const root = createRoot(container as Element);
 
--- mocks/handler.ts
+++ mocks/handler.ts
@@ -1,8 +1,43 @@
 import { BASE_URL } from './../api/index';
 import { rest } from 'msw';
 
+const getGroupList: Parameters<typeof rest.get>[1] = (req, res, ctx) => {
+  return res(
+    ctx.status(200),
+    ctx.json([
+      { title: '전국 노래 자랑', adminNickname: '윤하나둘셋넷', coverColor: '#f86565' },
+      { title: '전국 노래 자랑', adminNickname: '윤하나둘셋넷', coverColor: '#f86565' },
+      { title: '전국 노래 자랑', adminNickname: '윤하나둘셋넷', coverColor: '#f86565' },
+      { title: '전국 노래 자랑', adminNickname: '윤하나둘셋넷', coverColor: '#f86565' },
+      { title: '전국 노래 자랑', adminNickname: '윤하나둘셋넷', coverColor: '#f86565' },
+      { title: '전국 노래 자랑', adminNickname: '윤하나둘셋넷', coverColor: '#f86565' },
+      { title: '전국 노래 자랑', adminNickname: '윤하나둘셋넷', coverColor: '#f86565' },
+      { title: '전국 노래 자랑', adminNickname: '윤하나둘셋넷', coverColor: '#f86565' },
+      { title: '전국 노래 자랑', adminNickname: '윤하나둘셋넷', coverColor: '#f86565' },
+    ]),
+  );
+};
+
+const getGroupDetail: Parameters<typeof rest.get>[1] = (req, res, ctx) => {
+  return res(
+    ctx.status(200),
+    ctx.json([
+      {
+        message: '모임이 성공적으로 조회되었습니다.',
+        content: {
+          title: '전국 노래 자랑',
+          adminNickname: '윤하나둘셋넷',
+          createDate: '',
+          updateDate: '',
+          coverColor: '#f86565',
+          groupType: '학교, 교내/외 모임',
+        },
+      },
+    ]),
+  );
+};
+
 export const handler = [
-  rest.get(`${BASE_URL}`, (req, res, ctx) => {
-    return res(ctx.json([]));
-  }),
+  rest.get('/api/groupList', getGroupList), //
+  rest.get('/api/group/1', getGroupDetail), //
 ];
