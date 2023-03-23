27575252d91198bf04507854c4c54c37f818bf3d
김대은 <ko301201@naver.com>
2023-03-24 01:01:19 +0900

    [feat] UserConfigModal

---
+++ common/Modal/UserConfigModal/index.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from 'react';
+import Modal from '..';
+import * as Style from './styles';
+
+interface UserConfigModalProps {
+  handleModal: () => void;
+}
+
+const UserConfigModal: FC<UserConfigModalProps> = ({ handleModal }) => {
+  return (
+    <Modal.Frame width="493px" height="284px">
+      <Modal.Header align="start" onClick={handleModal}>
+        환경설정
+      </Modal.Header>
+      <Modal.Body>
+        <Style.Flex>
+          <Style.SubTitle>사용자 설정</Style.SubTitle>
+          <Style.ConfigContainer>
+            <Style.ContentWrapper>
+              <Style.Text>연동된 소셜 계정</Style.Text>
+              <Style.Kakao>카카오 간편 로그인</Style.Kakao>
+              <Style.Text>유저 이메일</Style.Text>
+              <Style.WithDrwalBtn>회원 탈퇴</Style.WithDrwalBtn>
+            </Style.ContentWrapper>
+          </Style.ConfigContainer>
+        </Style.Flex>
+      </Modal.Body>
+    </Modal.Frame>
+  );
+};
+
+export default UserConfigModal;
