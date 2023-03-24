4f33471f63ef01d71d3e518e3fcb4356ada88ac5
Jong1co <narira0922@gmail.com>
2023-03-25 00:36:57 +0900

    feature: 초대장 페이지 기능 완성
    - 로그인 정보 없을 시 로그인 모달, 로그인 정보가 있다면 입장권 모달

--- pages/Invitation/index.tsx
+++ pages/Invitation/index.tsx
@@ -4,27 +4,42 @@ import React, { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import * as Style from './styles';
 import { InvitationModal } from '@/common/Modal/InvitationModal';
+import { AuthModal } from '@/common/Modal/LoginModal';
 import { useGroupDetail } from '@/queries/Group';
 
 const Invitation = () => {
   const [showInvitationModal, setShowInvitationModal] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
   const user = useRecoilValue(userState);
 
   const handleInvitationModal = () => {
     setShowInvitationModal((prev) => !prev);
   };
 
+  const handleLoginModal = () => {
+    setShowLoginModal((prev) => !prev);
+  };
+
+  const checkUserLoginStatus = () => {
+    if (user.userId === null) {
+      handleLoginModal();
+    } else {
+      handleInvitationModal();
+    }
+  };
+
   const groupId = Number(window.location.search.split('=')[1]);
 
   const { data, isSuccess } = useGroupDetail({ groupId });
 
-  if (!isSuccess) return null;
+  if (!isSuccess) return <p>모임을 찾을 수 없습니다.</p>;
 
   return (
     <Style.Background>
-      <Button width="150px" height="42px" onClick={handleInvitationModal}>
+      <Button width="150px" height="42px" onClick={checkUserLoginStatus}>
         입장하기
       </Button>
+      {showLoginModal && <AuthModal modalHandler={handleLoginModal} />}
       {showInvitationModal && <InvitationModal groupName={data.content.title} onClick={handleInvitationModal} />}
     </Style.Background>
   );
