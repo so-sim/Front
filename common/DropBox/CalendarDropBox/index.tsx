93855088968eae22a5c7d6e345575a6b650b43ec
Jong1co <narira0922@gmail.com>
2023-04-05 17:09:44 +0900

    feature: 미니 캘린더 완성

--- common/DropBox/CalendarDropBox/index.tsx
+++ common/DropBox/CalendarDropBox/index.tsx
@@ -1,33 +1,40 @@
-import React, { Dispatch, FC, SetStateAction, useEffect, useState } from 'react';
+import React, { Dispatch, FC, SetStateAction, useEffect, useRef, useState } from 'react';
 import { ARROW } from '@/assets/icons/Arrow';
 import * as Style from '../styles';
-import { Calendar } from 'react-calendar';
-import 'react-calendar/dist/Calendar.css'; // css import
-import dayjs from 'dayjs';
+import MiniCalendar from './MiniCalendar';
 
 export type DropBoxColor = 'white' | 'gray' | 'disabled';
 
 interface CalendarDropBoxProps {
   type: string;
+  setType: Dispatch<SetStateAction<string>>;
   boxWidth?: string;
   width?: number;
   color?: DropBoxColor;
-  setType: Dispatch<SetStateAction<string>>;
   direction?: 'left' | 'right';
 }
 
-export const CalendarDropBox = <T,>({ setType, type, boxWidth = '138px', color = 'gray' }: CalendarDropBoxProps) => {
-  const [openDrop, setOpenDrop] = useState(false);
-  const [value, onChange] = useState<any>(new Date());
+export const CalendarDropBox = ({ setType, type, boxWidth = '138px', color = 'gray' }: CalendarDropBoxProps) => {
+  const dropDownRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    setType(dayjs(value).format('YYYY.MM.DD'));
-  }, [value]);
+  const [openDrop, setOpenDrop] = useState(false);
 
   const isDisabled = color === 'disabled';
 
+  useEffect(() => {
+    const onClickOutSide = (event: MouseEvent) => {
+      if (dropDownRef.current && !dropDownRef.current.contains(event.target as Node)) {
+        setOpenDrop(false);
+      }
+    };
+    document.addEventListener('mousedown', onClickOutSide);
+    return () => {
+      document.removeEventListener('mousedown', onClickOutSide);
+    };
+  }, []);
+
   return (
-    <Style.DropDownBox boxWidth={boxWidth} color={color}>
+    <Style.DropDownBox ref={dropDownRef} boxWidth={boxWidth} color={color}>
       <Style.Text boxWidth={boxWidth} isDisabled={isDisabled}>
         {(typeof type === 'string' && type) || '선택해주세요'}
       </Style.Text>
@@ -43,7 +50,7 @@ export const CalendarDropBox = <T,>({ setType, type, boxWidth = '138px', color =
 
       {openDrop && (
         <div style={{ position: 'absolute', top: '34px', left: '-2px' }}>
-          <Calendar value={value} onChange={onChange} />
+          <MiniCalendar type={type} setType={setType} setOpenDrop={setOpenDrop} />
         </div>
       )}
     </Style.DropDownBox>
