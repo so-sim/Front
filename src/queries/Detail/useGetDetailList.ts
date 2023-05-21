a568c8ed6c9bebe12d61488a694e4100ec59f840
Jong1co <narira0922@gmail.com>
2023-05-21 14:49:20 +0900

    fix : 요청에서 발생하는 에러 수정
    query가 생성되기 전에 요청이 들어가 발생하는 오류 enabled옵션 사용해서 수정

--- src/queries/Detail/useGetDetailList.ts
+++ src/queries/Detail/useGetDetailList.ts
@@ -1,13 +1,12 @@
 import { getEventList } from '@/api/Event';
-import { ToastPopUp } from '@/components/@common/Toast';
-import { TOAST_ERROR } from '@/constants/Toast';
-import { DateFilterProperty, dateFilterToQuery } from '@/utils/dateFilter';
+import { DateFilterProperty } from '@/utils/dateFilter/dateFilter';
 import { GroupId } from '@/types/group';
 import { useQuery } from '@tanstack/react-query';
 import { Dayjs } from 'dayjs';
+import { dateFilterToQuery } from '@/utils/dateFilterToQuery';
 
 export const useGetDetailList = (dateFilter: Partial<DateFilterProperty>, selectedDate: Dayjs | null, groupId: GroupId) => {
   const query = dateFilterToQuery(dateFilter);
 
-  return useQuery(['detailList', query, selectedDate, groupId.groupId], () => getEventList(query, groupId));
+  return useQuery(['detailList', query, selectedDate, groupId.groupId], () => getEventList(query, groupId), { enabled: !!query });
 };
