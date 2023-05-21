7201c6cc5658fad5c7081d2ccb874cc70017d061
Jong1co <narira0922@gmail.com>
2023-05-21 14:47:50 +0900

    chore : msw 해제

--- client.tsx
+++ client.tsx
@@ -3,11 +3,10 @@ import React from 'react';
 import App from './src/App';
 window.React = React;
 
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./src/mocks/browser');
-  worker.start();
-  console.log('hi');
-}
+// if (process.env.NODE_ENV === 'development') {
+//   const { worker } = require('./src/mocks/browser');
+//   worker.start();
+// }
 
 const container = document.getElementById('app');
 const root = createRoot(container as Element);
