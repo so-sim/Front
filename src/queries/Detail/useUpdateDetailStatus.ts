74d5fa9e3ff040a5281810379316ec49f28c7458
Jong1co <narira0922@gmail.com>
2023-05-25 22:27:08 +0900

    refactor : useUpdateDetailStatus
    - 외부에서 선언하던 onSuccess 함수 형태로 의존성 주입

--- src/queries/Detail/useUpdateDetailStatus.ts
+++ src/queries/Detail/useUpdateDetailStatus.ts
@@ -3,13 +3,14 @@ import { ToastPopUp } from '@/components/@common/Toast';
 import { TOAST_ERROR, TOAST_SUCCESS } from '@/constants/Toast';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-export const useUpdateDetailStatus = () => {
+export const useUpdateDetailStatus = (onSuccessUpdate?: () => void) => {
   const queryClient = useQueryClient();
   return useMutation(updateEventStatus, {
     onSuccess: () => {
       queryClient.invalidateQueries(['detailList']);
       queryClient.invalidateQueries(['monthStatus']);
       ToastPopUp({ type: 'success', message: TOAST_SUCCESS.UPDATE_FINE });
+      onSuccessUpdate && onSuccessUpdate();
     },
   });
 };
