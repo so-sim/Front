31f356a32068b8e7233a98944ef0e1331ecd86d5
Jong1co <narira0922@gmail.com>
2023-06-02 22:56:07 +0900

    chore: paragraph 컴포넌트 제거

--- src/components/Home/Paragraph/index.tsx
+++
@@ -1,6 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-import * as Style from './styles';
-
-export const Paragraph: React.FC<PropsWithChildren> = ({ children }) => {
-  return <Style.Paragraph>{children}</Style.Paragraph>;
-};
