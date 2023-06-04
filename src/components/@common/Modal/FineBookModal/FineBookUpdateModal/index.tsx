45f65a33486395fc5919afd7ceb437d9177fe03e
jonghyun <95389265+Jong1co@users.noreply.github.com>
2023-06-04 21:20:03 +0900

    Refactor: finebook modal 컴포넌트 create, update로 분리 (#98)
    * refactor: dropdown 리스트 가져오는 로직 분리
    
    * refactor: removeCommaFromPayment함수 의미있는 단위로 변경
    
    함수 이름 형식 일치시키기
    - convertFromPriceFormat
    - convertToPriceFormat
    
    * fix: Dispatch<SetState<T>> 타입 -> (value: T) =>void
    
    무조건 setState를 전달해야 해서 한번 더 가공해야 하는 것이 아니라 함수를 전달해서 쓸모 없는 state 생성 방지
    
    * style: inline css 제거
    
    * refactor: FineBookModal에서 중복되던 속성 컴포넌트로 분리
    
    * refactor: FineBookModal setState제거
    
    - 각각 속성마다 setState가 존재해 5개의 setState가 생성됨
    - 좀 더 편리한 관리를 위해 useReducer로 변경
    
    * feat: FineBookCreateModal, FineBookUpdateModal 생성
    
    * refactor: FineBookModal 삭제 및 create, update모달로 대체
    
    * refactor: 공통되는 reducer 따로 파일 분리
    
    * chore: 기본값 수정
    
    * refactor: checkFormIsValid함수 분리
    
    * chore: 변수명 수정
    
    * test: fineBookModal update, create버전 스토리북 작성

---
+++ src/components/@common/Modal/FineBookModal/FineBookUpdateModal/index.tsx
@@ -0,0 +1,65 @@
+import { Dispatch, SetStateAction, useReducer } from 'react';
+import Button from '@/components/@common/Button';
+import Modal from '@/components/@common/Modal';
+import * as Style from '../styles';
+import { useUpdateDetail } from '@/queries/Detail';
+import { ClientEventInfo, EventInfo } from '@/types/event';
+import { useRecoilState } from 'recoil';
+import dayjs from 'dayjs';
+import { dateState } from '@/store/dateState';
+import { getStatusCode, getStatusText } from '@/utils/status';
+import { ServerResponse } from '@/types/serverResponse';
+import FormFileds from '../FormFileds';
+import { selectedDataReducer } from '../reducer/selectedDataReducer';
+import { checkFormIsValid } from '@/utils/validation';
+
+interface Props {
+  modalHandler: () => void;
+  select: ClientEventInfo;
+  setSelect: Dispatch<SetStateAction<ClientEventInfo>>;
+}
+
+const FineBookUpdateModal = ({ modalHandler, select, setSelect }: Props) => {
+  const [selectData, dispatch] = useReducer(selectedDataReducer, select);
+
+  const [_, setDateState] = useRecoilState(dateState);
+
+  const onSuccessUpdateDetail = (data: ServerResponse<EventInfo>) => {
+    const groundsDate = dayjs(data.content.groundsDate);
+
+    setSelect((prev) => ({ ...prev, ...data.content, paymentType: getStatusText(selectData.paymentType) }));
+    setDateState((prev) => ({ ...prev, baseDate: groundsDate, selectedDate: groundsDate, week: null }));
+    modalHandler();
+  };
+
+  const { mutate: update, isLoading: updateLoading } = useUpdateDetail(onSuccessUpdateDetail);
+
+  const updateDetail = () => {
+    if (selectData.paymentType == '') return;
+    const { eventId, userId } = select;
+
+    update({ ...selectData, eventId, userId, paymentType: getStatusCode(selectData.paymentType) });
+  };
+
+  return (
+    <Modal.Frame width="448px" onClick={modalHandler}>
+      <Modal.Header onClick={modalHandler}>상세 내역 수정</Modal.Header>
+      <Style.FlexColumn>
+        <FormFileds dispatch={dispatch} selectData={selectData} />
+        <Modal.Footer>
+          <Button //
+            color={checkFormIsValid(selectData) ? 'black' : 'disabled'}
+            width="100%"
+            height="42px"
+            onClick={updateDetail}
+            loading={updateLoading}
+          >
+            저장하기
+          </Button>
+        </Modal.Footer>
+      </Style.FlexColumn>
+    </Modal.Frame>
+  );
+};
+
+export default FineBookUpdateModal;
--- src/utils/removeCommaFromPayment.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 금액(1,000,000)을 넘겨 받아서 콤마를 제거하는 함수
- */
-
-export const removeCommaFromPayment = (payment: string): number => {
-  return Number(payment.replaceAll(',', ''));
-};
