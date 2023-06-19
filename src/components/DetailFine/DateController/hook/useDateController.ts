fa1811c7ff1ab9a0632bce2a5a2437680b7de20d
Jong1co <narira0922@gmail.com>
2023-06-19 18:50:06 +0900

    feat: useDateController > goToWeek 함수 생성
    주차 선택 시 해당 주차로 baseDate를 업데이트하는 함수

--- src/components/DetailFine/DateController/hook/useDateController.ts
+++ src/components/DetailFine/DateController/hook/useDateController.ts
@@ -1,20 +1,39 @@
+import { FilterMode } from '@/pages/FineBook/DetailFine';
 import { dateStateTest } from '@/store/dateStateTest';
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import { useRecoilState } from 'recoil';
 
-const useDateController = () => {
+const useDateController = (mode: FilterMode) => {
   const [calendarDateTest, setSelectedDateTest] = useRecoilState(dateStateTest);
 
-  const weekToDate = (week: number) => {
-    const startOfMonthDay = dayjs(calendarDateTest.baseDateTest).set('date', 1).startOf('month').day();
-    const startOfWeekDate = (week - 1) * 7 + 1 - startOfMonthDay;
-
-    return startOfWeekDate;
+  const goToWeek = (week: number) => {
+    const baseDate = moveDateToWeek(calendarDateTest.baseDateTest, week);
+    setSelectedDateTest(baseDate);
   };
 
-  const action = { weekToDate };
+  const action = { goToWeek };
 
   return action;
 };
 
 export default useDateController;
+
+export function moveDateToWeek(baseDate: Dayjs, week: number) {
+  const startOfMonthDay = dayjs(baseDate).set('date', 1).startOf('month').day();
+  const startOfWeekDate = (week - 1) * 7 + 1 - startOfMonthDay;
+
+  if (week === 1) {
+    const startDateOfMonth = dayjs(baseDate).startOf('month');
+
+    return {
+      baseDateTest: startDateOfMonth, //
+      startDate: startDateOfMonth.startOf('week'),
+      endDate: startDateOfMonth.endOf('week'),
+    };
+  }
+
+  const startDate = dayjs(baseDate).set('date', startOfWeekDate);
+  const endDate = dayjs(baseDate).set('date', startOfWeekDate).endOf('week');
+
+  return { baseDateTest: startDate, startDate, endDate };
+}
