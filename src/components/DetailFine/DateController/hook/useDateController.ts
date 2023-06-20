e5c0305cfe39be4999c3b6bf9f518f8aa204c9bd
mini <jma1020@naver.com>
2023-06-20 15:22:25 +0900

    feat: increase, decrease 함수 작성 및 테스팅

--- src/components/DetailFine/DateController/hook/useDateController.ts
+++ src/components/DetailFine/DateController/hook/useDateController.ts
@@ -7,8 +7,9 @@ import { useRecoilState } from 'recoil';
 const useDateController = (mode: FilterMode) => {
   const [calendarDateTest, setSelectedDateTest] = useRecoilState(dateStateTest);
 
-  const goToWeek = (week: number) => {
-    const baseDate = moveDateToWeek(calendarDateTest.baseDateTest, week);
+  const goToWeek = (week: string) => {
+    const numberWeek = Number(week[0]);
+    const baseDate = moveDateToWeek(calendarDateTest.baseDateTest, numberWeek);
     setSelectedDateTest(baseDate);
   };
 
@@ -21,7 +22,15 @@ const useDateController = (mode: FilterMode) => {
     return getTitleByMode(calendarDateTest, mode);
   };
 
-  const action = { goToWeek, changeDateByButtonMode, getTitle };
+  const increase = () => {
+    return increaseTest(calendarDateTest.baseDateTest, mode);
+  };
+
+  const decrease = () => {
+    return increaseTest(calendarDateTest.baseDateTest, mode);
+  };
+
+  const action = { goToWeek, changeDateByButtonMode, getTitle, increase, decrease };
 
   return action;
 };
@@ -30,7 +39,10 @@ export default useDateController;
 
 export function moveDateToWeek(baseDate: Dayjs, week: number) {
   const startOfMonthDay = dayjs(baseDate).set('date', 1).startOf('month').day();
+  // 해당 달의 시작하는 요일을 찾는 것.
+  // day가 요일 date가 날짜.
   const startOfWeekDate = (week - 1) * 7 + 1 - startOfMonthDay;
+  // 선택된 week에 시작하는 날짜를 알아낼 수 있음.  (은근 날짜에 이쁜 패턴이 많다)
 
   if (week === 1) {
     const startDateOfMonth = dayjs(baseDate).startOf('month');
@@ -39,13 +51,14 @@ export function moveDateToWeek(baseDate: Dayjs, week: number) {
       baseDateTest: startDateOfMonth, //
       startDate: startDateOfMonth.startOf('week'),
       endDate: startDateOfMonth.endOf('week'),
+      mode: 'week' as FilterMode,
     };
   }
 
   const startDate = dayjs(baseDate).set('date', startOfWeekDate);
   const endDate = dayjs(baseDate).set('date', startOfWeekDate).endOf('week');
 
-  return { baseDateTest: startDate, startDate, endDate };
+  return { baseDateTest: startDate, startDate, endDate, mode: 'week' as FilterMode };
 }
 
 export function updateDateByButtonMode(baseDate: Dayjs, buttonMode: FilterMode) {
@@ -55,6 +68,7 @@ export function updateDateByButtonMode(baseDate: Dayjs, buttonMode: FilterMode) 
     baseDateTest: startDate,
     startDate: startDate.startOf(buttonMode),
     endDate: startDate.endOf(buttonMode),
+    mode: buttonMode,
   };
 }
 
@@ -74,6 +88,48 @@ function getBaseDateByChangedMode(baseDate: Dayjs, mode: FilterMode) {
     case 'day':
       return dayjs(baseDate);
   }
+}
+
+type ModeTilerType = {
+  [key: string]: Dayjs;
+};
+
+export function increaseTest(baseDate: Dayjs, mode: FilterMode) {
+  const startDayOfMonth = dayjs(baseDate).startOf('month');
+  const startDay = dayjs(baseDate).startOf('week');
+
+  const modeFilterObj: ModeTilerType = {
+    month: startDayOfMonth,
+    week: startDay.date() > dayjs(baseDate).date() ? startDayOfMonth : startDay,
+    day: dayjs(baseDate),
+  };
+  // 현재 이 부분을 어떻게 가져다 쓸지...
+  // 지금 현재 startDate endDate기반이 세워져있으니, 굳이 뺴야하나??  그냥 setDate prev.add(1,mode) 만 해줘도 될 것 같다.
+
+  return {
+    baseDateTest: modeFilterObj[mode].add(1, mode),
+    startDate: modeFilterObj[mode].add(1, mode),
+    endDate: modeFilterObj[mode].add(1, mode).endOf(mode),
+    mode,
+  };
+}
+
+export function decreaseTest(baseDate: Dayjs, mode: FilterMode) {
+  const startDayOfMonth = dayjs(baseDate).startOf('month');
+  const startDay = dayjs(baseDate).startOf('week');
+
+  const modeFilterObj: ModeTilerType = {
+    month: startDayOfMonth,
+    week: startDay.date() > dayjs(baseDate).date() ? startDayOfMonth : startDay,
+    day: dayjs(baseDate),
+  };
+
+  return {
+    baseDateTest: modeFilterObj[mode].subtract(1, mode),
+    startDate: modeFilterObj[mode].subtract(1, mode),
+    endDate: modeFilterObj[mode].subtract(1, mode).endOf(mode),
+    mode,
+  };
 }
 
 export function getTitleByMode(calendarDate: DateStateTest, mode: FilterMode) {
--- src/components/DetailFine/DateController/test/index.test.tsx
+++ src/components/DetailFine/DateController/test/index.test.tsx
@@ -1,7 +1,78 @@
+import { FilterMode } from '@/pages/FineBook/DetailFine';
 import dayjs from 'dayjs';
-import { getTitleByMode, moveDateToWeek, updateDateByButtonMode } from '../hook/useDateController';
+import { getTitleByMode, moveDateToWeek, updateDateByButtonMode, increaseTest, decreaseTest } from '../hook/useDateController';
 
 describe('DateController', () => {
+  describe('increaseDate', () => {
+    it('mode가 month이며 baseDate가 6월인 상태에서 실행 시, 7월로 변경이 되어야하며, endDate는 7월의 마지막 일로 지정되어야 한다..', () => {
+      const calendarDateState = increaseTest(dayjs('2023-06-17'), 'month');
+
+      expect(calendarDateState.baseDateTest.date()).toBe(1);
+      expect(calendarDateState.startDate.date()).toBe(1);
+      expect(calendarDateState.endDate.date()).toBe(31);
+      expect(calendarDateState.baseDateTest.month()).toBe(6);
+    });
+
+    it('mode가 week이며 baseDate가 6월 20일 상태에서 실행 시, startDate는 6월25일 endDate는 7월1일이여야한다.', () => {
+      const calendarDateState = increaseTest(dayjs('2023-06-20'), 'week');
+
+      expect(calendarDateState.baseDateTest.date()).toBe(25);
+      expect(calendarDateState.startDate.date()).toBe(25);
+      expect(calendarDateState.endDate.date()).toBe(1);
+    });
+
+    it('mode가 week이며 baseDate가 6월 28일 상태에서 실행 시, startDate는 7월2일 endDate는 7월 8일이여야한다.', () => {
+      const calendarDateState = increaseTest(dayjs('2023-06-28'), 'week');
+
+      expect(calendarDateState.baseDateTest.date()).toBe(2);
+      expect(calendarDateState.startDate.date()).toBe(2);
+      expect(calendarDateState.endDate.date()).toBe(8);
+    });
+
+    it('mode가 day이며 baseDate가 6월 29일 상태에서 실행 시, startDate는 6월30일 endDate는 6월30일이여야한다.', () => {
+      const calendarDateState = increaseTest(dayjs('2023-06-29'), 'day');
+
+      expect(calendarDateState.baseDateTest.date()).toBe(30);
+      expect(calendarDateState.startDate.date()).toBe(30);
+      expect(calendarDateState.endDate.date()).toBe(30);
+    });
+  });
+
+  describe('decreaseTest', () => {
+    it('mode가 month이며 baseDate가 6월인 상태에서 실행 시, 5월로 변경이 되어야하며, endDate는 5월의 마지막 일로 지정되어야 한다.', () => {
+      const calendarDateState = decreaseTest(dayjs('2023-06-29'), 'month');
+
+      expect(calendarDateState.baseDateTest.date()).toBe(1);
+      expect(calendarDateState.startDate.date()).toBe(1);
+      expect(calendarDateState.endDate.date()).toBe(31);
+      expect(calendarDateState.baseDateTest.month()).toBe(4);
+    });
+
+    it('mode가 week이며 baseDate가 6월 20일 상태에서 실행 시, startDate는 6월11일 endDate는 6월17일이여야 한다.', () => {
+      const calendarDateState = decreaseTest(dayjs('2023-06-20'), 'week');
+
+      expect(calendarDateState.baseDateTest.date()).toBe(11);
+      expect(calendarDateState.startDate.date()).toBe(11);
+      expect(calendarDateState.endDate.date()).toBe(17);
+    });
+
+    it('mode가 week이며 baseDate가 7월 3일 상태에서 실행 시, startDate는 6월25일 endDate는 7월 1일이여야 한다', () => {
+      const calendarDateState = decreaseTest(dayjs('2023-07-03'), 'week');
+
+      expect(calendarDateState.baseDateTest.date()).toBe(25);
+      expect(calendarDateState.startDate.date()).toBe(25);
+      expect(calendarDateState.endDate.date()).toBe(1);
+    });
+
+    it('mode가 day이며 baseDate가 6월 29일 상태에서 실행 시, startDate는 6월28일 endDate는 6월28일이여야한다.', () => {
+      const calendarDateState = decreaseTest(dayjs('2023-06-29'), 'day');
+
+      expect(calendarDateState.baseDateTest.date()).toBe(28);
+      expect(calendarDateState.startDate.date()).toBe(28);
+      expect(calendarDateState.endDate.date()).toBe(28);
+    });
+  });
+
   describe('moveDateToWeek', () => {
     it('6월의 1주차로 변경했을 때, 시작 날짜는 5월 28일 이어야한다.', () => {
       const calendarDateState = moveDateToWeek(dayjs('2023-06-19'), 1);
@@ -61,6 +132,7 @@ describe('DateController', () => {
         baseDateTest: dayjs('2023-06-19'),
         startDate: dayjs('2023-06-19'),
         endDate: dayjs('2023-06-19'),
+        mode: 'day' as FilterMode,
       };
       const title = getTitleByMode(dateState, 'day');
 
@@ -72,6 +144,7 @@ describe('DateController', () => {
         baseDateTest: dayjs('2023-06-19'),
         startDate: dayjs('2023-06-18'),
         endDate: dayjs('2023-06-24'),
+        mode: 'day' as FilterMode,
       };
       const title = getTitleByMode(dateState, 'week');
 
@@ -83,6 +156,7 @@ describe('DateController', () => {
         baseDateTest: dayjs('2023-06-01'),
         startDate: dayjs('2023-06-01'),
         endDate: dayjs('2023-06-30'),
+        mode: 'day' as FilterMode,
       };
       const title = getTitleByMode(dateState, 'month');
 
