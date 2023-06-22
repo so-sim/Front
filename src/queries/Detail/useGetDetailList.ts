c7d5c7f102c3ea4c19a13288f61f120f0ba97af3
mini <jma1020@naver.com>
2023-06-22 19:58:26 +0900

    fix: useGetDetailList  파라미터 type 수정

--- src/queries/Detail/useGetDetailList.ts
+++ src/queries/Detail/useGetDetailList.ts
@@ -1,9 +1,11 @@
 import { getEventList } from '@/api/Event';
-import { DetailFilter } from '@/utils/dateFilter/dateFilter';
+// import { DetailFilter } from '@/utils/dateFilter/dateFilter';
+
 import { GroupId } from '@/types/group';
 import { useQuery } from '@tanstack/react-query';
 import { Dayjs } from 'dayjs';
 import { dateFilterToQuery } from '@/utils/dateFilterToQuery';
+import { DetailFilter } from '@/store/detailFilter';
 
 export const useGetDetailList = (dateFilter: Partial<DetailFilter>, selectedDate: Dayjs | null, groupId: GroupId) => {
   const query = dateFilterToQuery(dateFilter);
