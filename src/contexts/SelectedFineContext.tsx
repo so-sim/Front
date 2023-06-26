6490197c0fb69d7d93451b2bbc75e70301d88771
mini <jma1020@naver.com>
2023-06-26 09:48:17 +0900

    refactor: 기존 select state 대신 사용할 SelectedFineContext 생성

---
+++ src/contexts/SelectedFineContext.tsx
@@ -0,0 +1,36 @@
+import { SelectedEventInfo } from '@/types/event';
+import dayjs from 'dayjs';
+import React, { createContext, Dispatch, SetStateAction, useContext, useState } from 'react';
+
+type SelectedFineState = {
+  selectedFine: SelectedEventInfo;
+  setSelectedFine: Dispatch<SetStateAction<SelectedEventInfo>>;
+};
+
+const SelectedFineContext = createContext<SelectedFineState | null>(null);
+
+export const initialSelectData: SelectedEventInfo = {
+  eventId: 0,
+  memo: '',
+  date: dayjs().format('YYYY.MM.DD'),
+  situation: '미납',
+  nickname: '',
+  amount: 0,
+  ground: '지각',
+};
+
+export function useSelectedContext(componentName: string) {
+  const context = useContext(SelectedFineContext);
+  if (!context) {
+    throw new Error(`<${componentName} /> is missing a parent <ReplyListContextProvider /> component.`);
+  }
+  return context;
+}
+
+const SelectedFineContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [selectedFine, setSelectedFine] = useState<SelectedEventInfo>(initialSelectData);
+
+  return <SelectedFineContext.Provider value={{ selectedFine, setSelectedFine }}>{children}</SelectedFineContext.Provider>;
+};
+
+export default SelectedFineContextProvider;
