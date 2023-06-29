450907ad92a8e408003bb184b0a96c696a853a13
Jong1co <narira0922@gmail.com>
2023-06-29 16:30:13 +0900

    style: DateController 스타일 수정

--- src/components/DetailFine/DateController/styles.ts
+++ src/components/DetailFine/DateController/styles.ts
@@ -5,7 +5,7 @@ export const DateController = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 12px;
+  margin-bottom: 8px;
 `;
 
 export const ArrowWrapper = styled.button`
