865cfe79cff959d5416ecc053b4bd650b83cc048
Jong1co <narira0922@gmail.com>
2023-07-01 20:43:03 +0900

    fix: 주간 필터링 시 날짜가 NaN으로 출력되는 버그 수정
    https://www.notion.so/333c5876c36f4c70a8e90bde13b7b482?pvs=4

--- src/utils/customedWeek.ts
+++ src/utils/customedWeek.ts
@@ -1,16 +1,25 @@
 import { GA } from '@/constants/GA';
 import dayjs, { Dayjs } from 'dayjs';
 
-const obj: Record<number, string> = {
-  1: '첫',
-  2: '둘',
-  3: '셋',
-  4: '넷',
-  5: '다섯',
-  6: '여섯',
+export const textOfWeek: Record<number, string> = {
+  1: '첫째 주',
+  2: '둘째 주',
+  3: '셋째 주',
+  4: '넷째 주',
+  5: '다섯째 주',
+  6: '여섯째 주',
+};
+
+export const numberOfWeek: Record<string, number> = {
+  '첫째 주': 1,
+  '둘째 주': 2,
+  '셋째 주': 3,
+  '넷째 주': 4,
+  '다섯째 주': 5,
+  '여섯째 주': 6,
 };
 
 export const customedWeek = (baseDate: Dayjs): { title: string; id: string }[] => {
   const endOfWeek = Math.ceil((dayjs(baseDate).startOf('month').day() + dayjs(baseDate).endOf('month').date()) / 7);
-  return new Array(endOfWeek).fill(0).map((_, i) => ({ title: `${obj[i + 1]}째 주`, id: GA.FILTER.WEEK_DROP }));
+  return new Array(endOfWeek).fill(0).map((_, i) => ({ title: `${textOfWeek[i + 1]}`, id: GA.FILTER.WEEK_DROP }));
 };
