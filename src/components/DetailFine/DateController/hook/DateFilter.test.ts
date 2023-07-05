125562ec144f0b794f8eb6297a38c11540a1c2cc
Jong1co <narira0922@gmail.com>
2023-07-05 18:44:33 +0900

    test: DayFilter 테스트코드 작성

--- src/components/DetailFine/DateController/hook/DateFilter.test.ts
+++ src/components/DetailFine/DateController/hook/DateFilter.test.ts
@@ -1,7 +1,7 @@
 import { DateState } from '@/store/dateState';
 import dayjs from 'dayjs';
-import { DateFilter } from './DateFilter';
-import { FilterModeTest, IDateFilter } from './useDateFilter';
+import { DateFilter, IDateFilter } from './DateFilter';
+import { FilterModeTest } from './useDateFilter';
 
 describe('customFilter', () => {
   let dateFilter: IDateFilter;
@@ -81,5 +81,56 @@ describe('customFilter', () => {
     expect(result.startDate.date()).toBe(3);
     expect(result.endDate.date()).toBe(31);
     expect(result.mode).toBe('custom');
+  });
+});
+
+describe('dayFilter', () => {
+  let dateFilter: IDateFilter;
+  const calendarDate: DateState = {
+    baseDate: dayjs('2023.07.01'),
+    startDate: dayjs('2023.07.01'),
+    endDate: dayjs('2023.07.01'),
+    mode: 'day',
+  };
+  beforeEach(() => {
+    dateFilter = new DateFilter('day') as any;
+  });
+
+  it('increaseDateByMode를 실행했을 때 다음 날이 필터링 되어야 한다.', () => {
+    const result = dateFilter.increaseDateByMode(calendarDate);
+
+    expect(result.baseDate.date()).toBe(2);
+    expect(result.startDate.date()).toBe(2);
+    expect(result.mode).toBe('day');
+  });
+
+  it('decreaseDateByMode를 실행했을 때 이전 날이 필터링 되어야 한다.', () => {
+    const result = dateFilter.decreaseDateByMode(calendarDate);
+
+    expect(result.baseDate.date()).toBe(30);
+    expect(result.startDate.date()).toBe(30);
+    expect(result.mode).toBe('day');
+  });
+
+  it('getTitle를 실행했을 때 제목은 00월 00일 포멧이어야 한다.', () => {
+    const result = dateFilter.getTitle(calendarDate);
+
+    expect(result).toBe('07월 01일');
+  });
+
+  it('updateDateByButtonMode를 실행했을 때 baseDate가 필터링 되어야 한다.', () => {
+    const calendarDate: DateState = {
+      baseDate: dayjs('2023.07.02'),
+      startDate: dayjs('2023.07.02'),
+      endDate: dayjs('2023.07.08'),
+      mode: 'week',
+    };
+
+    const result = dateFilter.updateDateByButtonMode(calendarDate, 'day');
+
+    expect(result.baseDate.date()).toBe(2);
+    expect(result.startDate.date()).toBe(2);
+    expect(result.endDate.date()).toBe(2);
+    expect(result.mode).toBe('day');
   });
 });
