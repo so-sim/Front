675e8a202a27c84ec3932a90a0f9df3f55c34593
Jong1co <narira0922@gmail.com>
2023-07-11 23:30:07 +0900

    feat: debouncing 적용

--- src/queries/Group/useSearchParticipantList.ts
+++ src/queries/Group/useSearchParticipantList.ts
@@ -1,6 +1,15 @@
 import { searchParticipantList } from '@/api/Group';
 import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 
 export const useSearchParticipantList = (groupId: number | undefined, keyword: string) => {
-  return useQuery(['participantList', groupId, keyword], () => searchParticipantList(groupId, keyword));
+  const [debouncedKeyword, setDebouncedKeyword] = useState('');
+  useEffect(() => {
+    let debounce = setTimeout(() => {
+      setDebouncedKeyword(keyword);
+    }, 150);
+
+    return () => clearTimeout(debounce);
+  }, [keyword]);
+  return useQuery(['participantList', groupId, debouncedKeyword], () => searchParticipantList(groupId, debouncedKeyword));
 };
