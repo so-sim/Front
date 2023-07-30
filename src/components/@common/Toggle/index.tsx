3a1c20d2f76f0f1b436d3c5b97e8d12687cc4ab2
Jong1co <narira0922@gmail.com>
2023-07-30 20:46:21 +0900

    fix: props 이름이 onToggle로 전달되어 warning 발생
    onToggle -> toggleState로 변경

--- src/components/@common/Toggle/index.tsx
+++ src/components/@common/Toggle/index.tsx
@@ -1,19 +1,14 @@
-import React, { Dispatch, SetStateAction } from 'react';
 import * as Style from './styles';
 
 type Props = {
-  onToggle: boolean;
-  setOnToggle: Dispatch<SetStateAction<boolean>>;
+  toggleState: boolean;
+  toggleHandler: () => void;
 };
 
-export const Toggle = ({ onToggle, setOnToggle }: Props) => {
-  const toggleHandler = () => {
-    setOnToggle((prev) => !prev);
-  };
-
+export const Toggle = ({ toggleState, toggleHandler }: Props) => {
   return (
-    <Style.Background onToggle={onToggle} onClick={toggleHandler}>
-      <Style.ToggleButton onToggle={onToggle} />
+    <Style.Background toggleState={toggleState} onClick={toggleHandler}>
+      <Style.ToggleButton toggleState={toggleState} />
     </Style.Background>
   );
 };
--- src/components/@common/Toggle/styles.ts
+++ src/components/@common/Toggle/styles.ts
@@ -1,17 +1,19 @@
 import styled from '@emotion/styled';
 
-export const Background = styled.button<{ onToggle: boolean }>`
+export const Background = styled.button<{ toggleState: boolean }>`
+  display: flex;
+  align-items: center;
   border-radius: 20px;
   width: 38px;
-  height: 18px;
-  background: ${({ theme, onToggle }) => (onToggle ? theme.colors.primary_500 : theme.colors.secondary_400)};
+  height: 20px;
+  background: ${({ theme, toggleState }) => (toggleState ? theme.colors.primary_500 : theme.colors.secondary_400)};
 `;
 
-export const ToggleButton = styled.div<{ onToggle: boolean }>`
+export const ToggleButton = styled.div<{ toggleState: boolean }>`
   width: 14px;
   height: 14px;
-  border-radius: 50%;
+  border-radius: 14px;
   background: ${({ theme }) => theme.colors.secondary_100};
-  transform: ${({ theme, onToggle }) => (onToggle ? 'translateX(22px)' : 'translateX(2px)')};
+  transform: ${({ theme, toggleState }) => (toggleState ? 'translateX(22px)' : 'translateX(2px)')};
   transition: ease-in-out 0.15s;
 `;
