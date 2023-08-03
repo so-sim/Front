11fdf987a47c3b7912fe4e803fc303ee1857d101
Jong1co <narira0922@gmail.com>
2023-08-03 16:56:00 +0900

    feat: card에 size 추가

--- src/components/Home/Card/index.tsx
+++ src/components/Home/Card/index.tsx
@@ -3,8 +3,13 @@ import * as Style from './styles';
 
 interface CardProps extends PropsWithChildren {
   onClick: () => void;
+  size: 'sm' | 'md';
 }
 
-export const Card: FC<CardProps> = ({ children, onClick }) => {
-  return <Style.Card onClick={onClick}>{children}</Style.Card>;
+export const Card: FC<CardProps> = ({ children, onClick, size }) => {
+  return (
+    <Style.Card onClick={onClick} size={size}>
+      {children}
+    </Style.Card>
+  );
 };
--- src/components/Home/Card/styles.ts
+++ src/components/Home/Card/styles.ts
@@ -1,10 +1,11 @@
 import styled from '@emotion/styled';
 
-export const Card = styled.li`
+export const Card = styled.li<{ size: 'sm' | 'md' }>`
   border-radius: 8px;
   background-color: ${({ theme }) => theme.colors.secondary_100};
   overflow: hidden;
   cursor: pointer;
   max-width: 220px;
-  height: 200px;
+  width: 100%;
+  height: ${({ size }) => (size === 'sm' ? '176px' : '200px')};
 `;
