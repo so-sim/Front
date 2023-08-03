2834aaaacf04cf315e8403db0fff311cd6839943
Jong1co <narira0922@gmail.com>
2023-08-03 13:58:44 +0900

    refactor: GNBLinkList 상수 폴더로 분리

--- src/constants/ServiceLink.ts
+++ src/constants/ServiceLink.ts
@@ -1,3 +1,5 @@
+import { GA } from './GA';
+
 export const TOS_LINK = {
   TERMS: 'https://www.notion.so/10e0ec59f6564133a9efbcf37e0c2c5a?pvs=4',
   PRIVACY: 'https://www.notion.so/10fd6054e69e4cdbb5e83548fce6f2d0?pvs=4',
@@ -8,6 +10,12 @@ export const GNB_LINK = {
   FAQ: 'https://www.notion.so/83ac133c02f2454fb4f6787aef123ce6?pvs=4',
   SUGGEST: 'https://forms.gle/V7ArjjrinyyHkvzT7',
 };
+
+export const GNBLinkList = [
+  { title: '서비스 소개', href: GNB_LINK.SERVICE, id: GA.INTRODUCTION },
+  { title: '의견 남기기', href: GNB_LINK.SUGGEST, id: GA.FEEDBACK },
+  { title: '이용 가이드', href: GNB_LINK.FAQ, id: GA.GUIDE },
+];
 
 export const FOOTER_LINK = {
   TERMS: 'https://www.notion.so/10e0ec59f6564133a9efbcf37e0c2c5a?pvs=4',
