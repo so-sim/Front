8b76821f3f0b204b8d387fc1d1645024f6f29e88
Jong1co <narira0922@gmail.com>
2023-08-03 12:38:24 +0900

    refactor: groupList 혹으로 분리

--- src/components/Home/CardList/index.tsx
+++ src/components/Home/CardList/index.tsx
@@ -1,23 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { AddCard } from '../Card/AddCard';
 import { GroupCard } from '../Card/GroupCard';
 import { CreateGroupModal } from '@/components/@common/Modal/CreateGroupModal';
 import * as Style from './styles';
-import { useGroupList } from '@/queries/Group/';
 import { userState } from '@/store/userState';
 import { useRecoilValue } from 'recoil';
 import { AuthModal } from '@/components/@common/Modal/LoginModal';
-import { useInView } from 'react-intersection-observer';
 import React from 'react';
 import { GA } from '@/constants/GA';
+import useInfinityGroupList from '@/hooks/Group/useInfinityGroupList';
 
 export const CardList = () => {
   const { userId } = useRecoilValue(userState);
   const [showCreateGroupModal, setShowCreateGroupModal] = useState(false);
   const [showLoginModal, setShowLoginModal] = useState(false);
-  const { ref, inView } = useInView();
 
-  const { data: groups, fetchNextPage, hasNextPage } = useGroupList();
+  const { ref, groups } = useInfinityGroupList();
 
   const handleCreateGroupModal = () => {
     const isLoggedIn = userId !== null;
@@ -27,12 +25,6 @@ export const CardList = () => {
       setShowLoginModal((prev) => !prev);
     }
   };
-
-  useEffect(() => {
-    if (inView && hasNextPage) {
-      fetchNextPage();
-    }
-  }, [inView]);
 
   return (
     <>
