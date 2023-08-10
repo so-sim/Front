783cb4d411343f7bf3275b1fc38866f66dba4e04
mini <jma1020@naver.com>
2023-08-11 02:46:02 +0900

    feat: useCheckListState 생성으로 인한 코드구조 변경(대부분의 checkbox props  제거)

--- src/pages/FineBook/DetailFine/index.tsx
+++ src/pages/FineBook/DetailFine/index.tsx
@@ -3,15 +3,17 @@ import { useGetDetailList } from '@/queries/Detail/useGetDetailList';
 import { SelectedEventInfo } from '@/types/event';
 import { DateController, DetailList, DetailsHeader, FilterController, Pagination, TableHead, UserDetails } from '@/components/DetailFine';
 import * as Style from './styles';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { useParams } from 'react-router-dom';
 
 import dayjs from 'dayjs';
 import { dateState } from '@/store/dateState';
 import { DetailFilter } from '@/store/detailFilter';
 import SelectedFineContextProvider from '@/contexts/SelectedFineContext';
-import useCheckDetailFine from '@/components/DetailFine/AlarmRequest_PaymentUpdate/hooks/useCheckDetailFine';
+
 import AlarmRequest_PaymentUpdate from '@/components/DetailFine/AlarmRequest_PaymentUpdate';
+
+import useCheckListState from '@/hooks/useCheckListState';
 
 export type FilterMode = 'month' | 'week' | 'day';
 
@@ -33,10 +35,11 @@ const DetailFine = () => {
   const calendarDate = useRecoilValue(dateState);
   const { data } = useGetDetailList(detailFilter, calendarDate);
 
-  const { checkDetailFine, setCheckDetailFine } = useCheckDetailFine();
-  // 현재 걍 context쓸지 고민 중 (checkDetailFine과 paymentControl - type 관련 )
-
-  const { setInitCheckDetailFine } = setCheckDetailFine;
+  const {
+    checkDetailFineList,
+    checkedEventIdList,
+    setCheckDetailFine: { setInitCheckDetailFine },
+  } = useCheckListState();
 
   useEffect(() => {
     setInitCheckDetailFine();
@@ -49,12 +52,12 @@ const DetailFine = () => {
         <Style.DetailContent>
           <DateController setDetailFilter={setDetailFilter} />
           <FilterController detailFilter={detailFilter} setDetailFilter={setDetailFilter} totalAmount={1_000_000} />
-          <TableHead setDetailFilter={setDetailFilter} details={data?.content.eventList} checkDetailFine={checkDetailFine} setCheckDetailFine={setCheckDetailFine} />
-          <DetailList detailFilter={detailFilter} details={data?.content.eventList} checkDetailFine={checkDetailFine} setCheckDetailFine={setCheckDetailFine} />
+          <TableHead setDetailFilter={setDetailFilter} details={data?.content.eventList} />
+          <DetailList detailFilter={detailFilter} details={data?.content.eventList} />
         </Style.DetailContent>
         <Pagination totalCount={data?.content.totalCount} detailFilter={detailFilter} setDetailFilter={setDetailFilter} />
         <UserDetails />
-        <AlarmRequest_PaymentUpdate checkDetailFine={Object.values(checkDetailFine)} setCheckDetailFine={setCheckDetailFine} />
+        <AlarmRequest_PaymentUpdate checkDetailFineList={[...checkDetailFineList]} checkedEventIdList={[...checkedEventIdList]} />
       </Style.DetailFineFrame>
     </SelectedFineContextProvider>
   );
