96d2151ae1349f66f9d8dbb6d20e4620583cbcb9
Jong1co <narira0922@gmail.com>
2023-08-13 21:39:16 +0900

    refactor: 변수 이름 통일

--- src/m-pages/MobileSetting/index.tsx
+++ src/m-pages/MobileSetting/index.tsx
@@ -1,13 +1,12 @@
 import { ARROW } from '@/assets/icons/Arrow';
 import ModalPageLayout from '@/layouts/Mobile/ModalPageLayout';
-import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 type SettingType = 'group' | 'alarm';
 
-const SETTING_LIST: { label: string; value: SettingType }[] = [
-  { label: '사용자 설정', value: 'group' },
-  { label: '알림 설정', value: 'alarm' },
+const SETTING_LIST: { value: string; type: SettingType }[] = [
+  { value: '사용자 설정', type: 'group' },
+  { value: '알림 설정', type: 'alarm' },
 ];
 
 const MobileSetting = () => {
@@ -25,9 +24,9 @@ const MobileSetting = () => {
   return (
     <ModalPageLayout left={{ icon: ARROW.LEFT, onClick: goBack }} title="모임 설정">
       <div style={{ margin: '20px 16px 0 16px' }}>
-        {SETTING_LIST.map((setting) => (
-          <div key={setting.value} onClick={() => moveToSettingPage(setting.value)}>
-            {setting.label}
+        {SETTING_LIST.map(({ type, value }) => (
+          <div key={value} onClick={() => moveToSettingPage(type)}>
+            {value}
           </div>
         ))}
       </div>
