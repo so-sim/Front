50eaaffefb9a9d8a283db03abf9e0ecec17c1e13
mini <jma1020@naver.com>
2023-08-14 20:12:48 +0900

    style: 초대장 - 이름설정 모달 Text 사이즈 수정

--- src/components/@common/Label/styles.ts
+++ src/components/@common/Label/styles.ts
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { CSSProperties } from 'react';
 
@@ -8,13 +9,19 @@ export const Label = styled.label<{ flexDirection: CSSProperties['flexDirection'
   margin-bottom: ${(props) => props.margin};
 `;
 
-export const LabelText = styled.div<{ flexDirection: CSSProperties['flexDirection']; width: string }>`
+export const LabelText = styled.div<{ flexDirection: CSSProperties['flexDirection']; width: string; $isMobile: boolean }>`
   ${({ theme }) => theme.font.subhead_03}
   margin: 8px 8px ${({ flexDirection }) => (flexDirection === 'row' ? '0 0' : '4px 4px')};
   width: ${({ width }) => width};
   display: flex;
   white-space: nowrap;
   justify-content: ${({ flexDirection }) => (flexDirection === 'row' ? 'flex-end' : 'flex-start')};
+
+  ${({ $isMobile, theme }) =>
+    $isMobile &&
+    css`
+      ${theme.font.subhead_02}
+    `}
 `;
 
 export const ArrangeRow = styled.div`
