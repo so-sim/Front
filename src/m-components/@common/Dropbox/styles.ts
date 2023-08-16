1e1170033461a944083ca633d0ebeb6feedb4aca
Jong1co <narira0922@gmail.com>
2023-08-16 21:21:17 +0900

    feat: MobileDropbox 생성

---
+++ src/m-components/@common/Dropbox/styles.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Dropbox = styled.button<{ active: boolean; width: string }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: ${({ width }) => width};
+  padding: 4px 12px;
+  border-radius: 4px;
+  color: ${({ theme, active }) => (active ? theme.colors.secondary_900 : theme.colors.secondary_600)};
+  border: 1px solid ${({ theme }) => theme.colors.secondary_400};
+  ${({ theme }) => theme.font.body_02};
+`;
