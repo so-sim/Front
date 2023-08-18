0c036702cdcd95f23a64f4d0855674307b463e02
Jong1co <narira0922@gmail.com>
2023-08-18 21:13:22 +0900

    fix: useRedirect hook 수정
    등록되지 않은 url 중 공통으로 사용되는 경로일 경우, common으로 할당해, reidrect시키지 않도록 수정

--- src/hooks/@common/useRedirectURL.ts
+++ src/hooks/@common/useRedirectURL.ts
@@ -2,8 +2,10 @@ import React, { useEffect } from 'react';
 import { isMobile } from 'react-device-detect';
 import { useLocation } from 'react-router-dom';
 
-type Device = 'mobile' | 'desktop';
+type Device = 'mobile' | 'desktop' | 'common';
 
+// isMobile은 크롬에서 모바일 -> 데스크탑으로 변경했을 때, 감지하지 못함
+// 즉, 초기값을 계속 유지하는 거 같음 마치 localStorage처럼
 const useRedirectURL = () => {
   const location = useLocation();
 
@@ -11,31 +13,33 @@ const useRedirectURL = () => {
     const splitedPathname = location.pathname.split('/');
     const equalPathBundle = findEqualPathBundle(splitedPathname);
 
-    const { bundle, comparedDevice } = equalPathBundle;
+    const { bundle, comparedDevice, params } = equalPathBundle;
 
     // 같은 디바이스일 경우 리다이렉트가 필요 없음 (모바일인 경우 경로가 "/m-home" 이라면 리다이렉트 필요 x)
     if (isSameDevice(comparedDevice)) return;
 
+    // 둘 다 공통으로 사용되는 auth와 같은 페이지
+    if (comparedDevice === 'common') return;
     const [desktop, mobile] = bundle;
 
     // 디바이스가 모바일인 경우, 모바일 경로로 리다이렉트
     if (isMobile) {
-      redirectToCorrectUrl(mobile);
+      redirectToCorrectUrl(mapParams(mobile, params));
     } else {
-      redirectToCorrectUrl(desktop);
+      redirectToCorrectUrl(mapParams(desktop, params));
     }
-  }, [location.pathname]);
+  }, [location]);
 };
 
 export default useRedirectURL;
 
-const redirectToCorrectUrl = (url: string) => {
-  window.location.href = process.env.REACT_APP_SERVICE_URL + url;
-};
+const mapParams = (url: string, params: string[]) => {
+  let mappedUrl = url;
+  for (let param of params) {
+    mappedUrl = mappedUrl.replace('*', param);
+  }
 
-const isSameDevice = (comparedDevice: Device) => {
-  const device: Device = isMobile ? 'mobile' : 'desktop';
-  return device === comparedDevice;
+  return mappedUrl;
 };
 
 // 현재 URL과 같은 경로를 가진 URL bundle을 찾는 함수
@@ -46,31 +50,40 @@ const findEqualPathBundle = (splitedPathname: string[]) => {
   for (const splitedUrl of splitedUrlList) {
     index++;
     if (splitedUrl.length !== splitedPathname.length) continue;
+    const params: string[] = [];
 
-    if (isEqual(splitedUrl, splitedPathname)) {
+    if (isEqual(splitedUrl, splitedPathname, params)) {
       const targetIndex = Math.floor(index / 2);
       const comparedDevice: Device = index % 2 === 1 ? 'mobile' : 'desktop';
 
-      return { bundle: urlList[targetIndex], comparedDevice };
+      return { bundle: urlList[targetIndex], comparedDevice, params };
     }
   }
 
-  throw new Error('404 Page Not Found: 존재하지 않는 URL입니다. useRedirectURL을 확인해주세요');
+  return { bundle: ['', ''], comparedDevice: 'common' as Device, params: [] };
+  // throw new Error('404 Page Not Found: 존재하지 않는 URL입니다. useRedirectURL을 확인해주세요');
 };
 
 /**
  * 두 개의 배열이 같은지 확인하는 함수 (와일드카드일 경우 분기 처리)
  */
-const isEqual = (arr1: string[], arr2: string[]) => {
+const isEqual = (arr1: string[], arr2: string[], params: string[]) => {
   if (arr1.length !== arr2.length) return false;
+
   for (let i = 0; i < arr1.length; i++) {
     if (!(arr1[i] === arr2[i] || arr1[i] === '*')) return false;
+    if (arr1[i] === '*') params.push(arr2[i]);
   }
 
   return true;
 };
 
-const urlList: [home: string, mobile: string][] = [
+/**
+ * 좌측: 데스크탑 url
+ * 우측: 모바일 url
+ * 와일드카드는 *로 표시
+ */
+const urlList: [desktop: string, mobile: string][] = [
   ['/', '/m-home'],
   ['/', '/m-setting'],
   ['/', '/m-home/create-group'],
@@ -87,3 +100,12 @@ const urlList: [home: string, mobile: string][] = [
   ['/group/*/book/detail', '/m-group/*/group-setting/create-finebook'],
   ['/group/*/book/detail', '/m-group/*/group-setting/update-finebook'],
 ];
+
+const redirectToCorrectUrl = (url: string) => {
+  window.location.href = process.env.REACT_APP_SERVICE_URL + url;
+};
+
+const isSameDevice = (comparedDevice: Device) => {
+  const device: Device = isMobile ? 'mobile' : 'desktop';
+  return device === comparedDevice;
+};
