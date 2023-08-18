4e5ae2269a7224714dea07714a67296050514214
mini <jma1020@naver.com>
2023-08-18 23:06:47 +0900

    feat: 상세페이지 및 납부여부 변경 및 알람요청 페이지 Route 추가

--- src/routes/MobileRouter.tsx
+++ src/routes/MobileRouter.tsx
@@ -14,6 +14,8 @@ import MobileDetailFine from '@/m-components/MobileDetailFine';
 import MobileMemberSearch from '@/m-pages/MobileMemberSearch';
 import MobileCreateFineBook from '@/m-pages/MobileCreateFineBook';
 import MobileUpdateFineBook from '@/m-pages/MobileUpdateFineBook';
+import MobileFineBookDetail from '@/m-pages/MobileFineBookDetail';
+import MobileAlarmRequest_Payment from '@/m-pages/MobileAlarmRequest_Payment';
 
 const MobileRouter = () => {
   return (
@@ -27,11 +29,13 @@ const MobileRouter = () => {
         <Route path="/m-group/:groupId/book" element={<MobileCalendar />} />
         <Route path="/m-group/:groupId/book/member-search" element={<MobileMemberSearch />} />
         <Route path="/m-group/:groupId/book/detail" element={<MobileDetailFine />} />
+        <Route path="/m-group/:groupId/book/detail/:fineBookDetailId" element={<MobileFineBookDetail />} />
         <Route path="/m-group/:groupId/group-setting" element={<MobileSetting />} />
         <Route path="/m-group/:groupId/group-setting/group" element={<MobileGroupSetting />} />
         <Route path="/m-group/:groupId/group-setting/alarm" element={<MobileAlarmSetting />} />
         <Route path="/m-group/:groupId/create-finebook" element={<MobileCreateFineBook />} />
         <Route path="/m-group/:groupId/update-finebook" element={<MobileUpdateFineBook />} />
+        <Route path="/m-group/:groupId/book/alarm" element={<MobileAlarmRequest_Payment />} />
         <Route path="/m-tos" element={<MobileTOS />} />
         <Route path="/m-setting" element={<MobileUserSetting />} />
         <Route path="/m-withdrawal" element={<MobileWithdrawal />} />
