0feaff9d594dd531964005358ef0ab98a42e15b6
Jong1co <narira0922@gmail.com>
2023-08-20 21:55:43 +0900

    feat: isOverHeight 함수 사용하면 예외 페이지 추가 안 해도 되긴 함
    그러나 발동 시점이 애매함.
    데이터를 불러온 후에 height를 판단해야 하는데, 데이터를 불러오기 이전에 판단해 대체로 false로 나옴
    - 해결 필요

--- src/hooks/@common/useWindowHeight.ts
+++ src/hooks/@common/useWindowHeight.ts
@@ -6,6 +6,19 @@ const isExceptionPath = (path: string) => {
   return EXCEPTION_PATH.includes(path);
 };
 
+const isOverHeight = () => {
+  const scrollHeight = Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.body.clientHeight,
+    document.documentElement.clientHeight,
+  );
+
+  return window.innerHeight < scrollHeight;
+};
+
 const useWindowHeight = () => {
   const windowRef = useRef<HTMLDivElement>(null);
   const location = useLocation();
