598f9e78de55c464b6e849c0ef9f545c0de84c06
mini <jma1020@naver.com>
2023-08-24 15:14:46 +0900

    style: 알람 카운트 style 추가

--- src/components/Alarm/styles.ts
+++ src/components/Alarm/styles.ts
@@ -1,16 +1,22 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
-export const AlarmIconWrapper = styled.div`
+export const AlarmIconWrapper = styled.div<{ $isCount?: boolean }>`
   position: relative;
-  &::after {
-    content: '99+';
 
-    position: absolute;
-    padding: 2px 9px;
-    background-color: red;
-    color: white;
-    border-radius: 1.25rem;
-    top: -50%;
-    left: 25%;
-  }
+  ${({ $isCount }) =>
+    $isCount &&
+    css`
+      &::after {
+        content: attr(data-count);
+
+        position: absolute;
+        padding: 2px 9px;
+        background-color: red;
+        color: white;
+        border-radius: 1.25rem;
+        top: -50%;
+        left: 25%;
+      }
+    `}
 `;
