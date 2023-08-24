885195aa908946d3f20076ec42cee6bc826d9049
mini <jma1020@naver.com>
2023-08-24 15:18:01 +0900

    feat: 알람 상세내역 컴포넌트 생성 및 스타일 추가

--- src/components/Alarm/AlarmDetail/AlarmInfo/styles.ts
+++ src/components/Alarm/AlarmDetail/AlarmInfo/styles.ts
@@ -1,4 +1,8 @@
+import { SituationStatus } from '@/types/notification';
 import styled from '@emotion/styled';
+import { Situation } from '@/types/event';
+import { DefaultTheme } from '@/styles/Theme';
+import { css } from '@emotion/react';
 
 export const Title = styled.h2`
   padding-bottom: 0.5rem;
@@ -34,4 +38,55 @@ export const SubTitle = styled.p`
   ${({ theme }) => theme.font.body_03};
 
   color: ${({ theme }) => theme.colors.secondary_900};
+`;
+
+export const SITUATION_USERLINESTYLES = {
+  미납: (theme: DefaultTheme) => css`
+    color: ${theme.colors.red_400};
+  `,
+  확인중: (theme: DefaultTheme) => css`
+    color: ${theme.colors.orange_600};
+  `,
+  완납: (theme: DefaultTheme) => css`
+    color: ${theme.colors.primary_400};
+  `,
+};
+
+export const UserLineSpan = styled.span<{ $situation: Situation }>`
+  text-decoration: underline;
+  ${({ theme, $situation }) => SITUATION_USERLINESTYLES[$situation](theme)};
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 12px;
+
+  position: absolute;
+
+  bottom: 0;
+  right: 0;
+  left: 0;
+
+  padding: 0 24px;
+  padding-bottom: 48px;
+
+  background-color: white;
+`;
+
+export const Button = styled.button<{ isSubmit?: boolean }>`
+  flex: 1;
+  padding: 11px 0;
+
+  box-shadow: inset 0 0 0 1px ${({ theme }) => theme.colors.secondary_800};
+  border-radius: 0.25rem;
+
+  ${({ theme }) => theme.font.subhead_02};
+
+  ${({ isSubmit, theme }) =>
+    isSubmit &&
+    css`
+      background-color: ${theme.colors.secondary_800};
+      color: ${theme.colors.secondary_100};
+    `}
 `;
