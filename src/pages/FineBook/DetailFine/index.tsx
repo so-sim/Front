4979005c712d447c93fa97739ed89a3cefa48f01
mini <jma1020@naver.com>
2023-08-26 16:11:27 +0900

    feat: 알람 클릭 시 보이는 View 스타일 생성 및 알람 data 삽입하는 코드

--- src/pages/FineBook/DetailFine/index.tsx
+++ src/pages/FineBook/DetailFine/index.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useGetDetailList } from '@/queries/Detail/useGetDetailList';
-import { SelectedEventInfo } from '@/types/event';
+import { SelectedEventInfo, Situation } from '@/types/event';
 import { DateController, DetailList, DetailsHeader, FilterController, Pagination, TableHead, UserDetails } from '@/components/DetailFine';
 import * as Style from './styles';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 import dayjs from 'dayjs';
 import { dateState } from '@/store/dateState';
@@ -15,6 +15,9 @@ import AlarmRequest_PaymentUpdate from '@/components/DetailFine/AlarmRequest_Pay
 
 import useCheckListState from '@/hooks/useCheckListState';
 import { sideModalState } from '@/store/sideModalState';
+import { alarmInfoState } from '@/store/alarmInfoState';
+import { SituationStatus } from '@/types/notification';
+import styled from '@emotion/styled';
 
 export type FilterMode = 'month' | 'week' | 'day';
 
@@ -28,6 +31,11 @@ export const initialSelectData: SelectedEventInfo = {
   ground: '지각',
 };
 
+const SITUATION_FORMAT_STYLE: { [key in SituationStatus]: Situation } = {
+  FULL: '완납',
+  CHECK: '확인중',
+  NONE: '미납',
+};
 const DetailFine = () => {
   const { groupId } = useParams();
 
@@ -49,6 +57,18 @@ const DetailFine = () => {
     setInitCheckDetailFine();
   }, [calendarDate]);
 
+  const [alarmState, setAlarmEventIdList] = useRecoilState(alarmInfoState);
+
+  const [searchParam, setSearhParam] = useSearchParams();
+
+  useEffect(() => {
+    setDetailFilter({ nickname: '', situation: '', page: 0, size: 16, groupId: Number(groupId) });
+    if (searchParam.get('isnotification')) {
+      setDetailFilter((prev) => ({ ...prev, nickname: alarmState.nickname, situation: SITUATION_FORMAT_STYLE[alarmState.afterSituation || 'NONE'] }));
+      // 닉네임과 납부여부 설정하는 곳인데/  닉네임설정하려면 input 창 맨처음에 설정해야함
+    }
+  }, [groupId, alarmState, searchParam]);
+
   // check를 다시 풀고 다시 해줘야 User인터랙션이 겹쳤을 때 바뀐 상태를 다시 저장할 수 있다.  (그래서 그냥 다시 요청을 보내기로)
 
   return (
@@ -69,9 +89,23 @@ const DetailFine = () => {
             <AlarmRequest_PaymentUpdate checkDetailFine={{ ...checkDetailFine }} />
           </AlarmRequest_PaymentUpdate.DesktopFrame>
         )}
+        {searchParam.get('isnotification') && <BackDrop />}
       </Style.DetailFineFrame>
     </SelectedFineContextProvider>
   );
 };
 
 export default DetailFine;
+
+export const BackDrop = styled.div`
+  position: absolute;
+  top: 68px;
+  bottom: 0;
+  right: 0;
+  left: 0;
+
+  background-color: #2d2d2d;
+  opacity: 30%;
+
+  z-index: 9998;
+`;
