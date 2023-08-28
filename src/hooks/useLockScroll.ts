acca52baf2bea61ee916d153529fbcabe058a728
mini <jma1020@naver.com>
2023-08-28 22:26:11 +0900

    feat: scrolList 생성으로 관리 형태 변경으로 인한 useLockScroll 수정

--- src/hooks/useLockScroll.ts
+++ src/hooks/useLockScroll.ts
@@ -1,8 +1,20 @@
+import { lockScrollState } from '@/store/lockScrollState';
 import { useEffect } from 'react';
 import { isMobile } from 'react-device-detect';
+import { useRecoilState } from 'recoil';
 
 // 아예 트리거를 하는 state 까지 가지고 return 해주면 좋을 것 같아 논의사항
-const useLockScroll = (trigger: boolean) => {
+const useLockScroll = (trigger: boolean, name: string) => {
+  const [lockList, setLockList] = useRecoilState(lockScrollState);
+
+  useEffect(() => {
+    if (trigger) {
+      setLockList((prev) => new Set(prev.add(name)));
+    } else {
+      setLockList((prev) => new Set([...prev].filter((id) => id !== name)));
+    }
+  }, [trigger]);
+
   const blockScroll = () => {
     const html = document.documentElement;
     const { body } = document;
@@ -32,8 +44,8 @@ const useLockScroll = (trigger: boolean) => {
   };
 
   useEffect(() => {
-    trigger ? blockScroll() : allowScroll();
-  }, [trigger]);
+    lockList.size > 0 ? blockScroll() : allowScroll();
+  }, [trigger, lockList]);
 };
 
 export default useLockScroll;
