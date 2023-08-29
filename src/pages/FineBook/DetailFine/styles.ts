b31396fb3b4d356f777961a198542d70244beb5e
mini <jma1020@naver.com>
2023-08-29 16:39:02 +0900

    style: 불필요한 코드 제거?? 추 후 확인필요

--- src/pages/FineBook/DetailFine/styles.ts
+++ src/pages/FineBook/DetailFine/styles.ts
@@ -5,11 +5,13 @@ export const DetailFineFrame = styled.section`
   /* relative 주게되면 다 깨진다... 이거 나중에 absolute가 추가될 수도 있어서 수정해야하려나?? */
 
   border-left: 2px solid ${({ theme }) => theme.colors.neutral_200_b};
-  height: calc(100vh - 68px);
-  overflow: auto;
+  /* height: calc(100vh - 68px);
+  overflow: auto; */
 `;
 
 export const DetailContent = styled.div`
   padding: 28px 32px;
   min-width: 784px;
+
+  /* height: calc(100vh - 200px); */
 `;
