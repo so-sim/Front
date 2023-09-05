7f0b08ad6596b87a21155758b0c8a43b3ea44256
mini <jma1020@naver.com>
2023-09-06 01:00:15 +0900

    feat: GA에 맞는 값을 보내기 위한 포맷변경 상수 선언

---
+++ src/constants/GA_ALARM.ts
@@ -0,0 +1,14 @@
+export const GA_ALARM_TYPE = {
+  PAYMENT_DATE: 'auto_request',
+  REQUEST_PAYMENT: 'payment_request',
+  CHANGE_FULL_SITUATION: 'fullpayment',
+  CHANGE_NONE_SITUATION: 'nonpayment',
+  CHANGE_CHECK_SITUATION: 'confirming',
+  CHANGE_ADMIN: 'admin_change',
+};
+
+export const GA_ALARM_SITUATION = {
+  미납: 'nonpayment',
+  확인중: 'confirming',
+  완납: 'fullpayment',
+};
