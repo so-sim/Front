22c553592bbd435cf047442dbb8904e56f92ceca
mini <jma1020@naver.com>
2023-10-09 18:43:20 +0900

    fix: 인스턴스  interceptor response 에러핸들링 수정(accessToken 유지)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,11 @@
+import { testState } from '@/store/test';
 import { ServerResponse } from '@/types/serverResponse';
 import { getAccessToken, isErrorCase, removeAccessToken } from '@/utils/acceessToken';
 import axios, { AxiosError } from 'axios';
+import { useRecoilState } from 'recoil';
 import { reTakeToken } from './Auth';
 import { notFoundGroupDetail } from './Error';
+import { deleteEvent } from './Event';
 
 export const BASE_URL = process.env.REACT_APP_SERVER_URL;
 
@@ -13,6 +16,9 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const accessToken = getAccessToken();
+  if (config.url === '/api/event/penalty/73') {
+    console.log(config);
+  }
   if (config.headers && accessToken) {
     config.headers.Authorization = `Bearer ${accessToken}`;
   }
@@ -21,30 +27,43 @@ api.interceptors.request.use((config) => {
 
 let lock = false;
 
+/**
+ * 서버 오류 시 무한 재귀를 대응하기위한 count 변수
+ * */
+let count = 0;
+
 api.interceptors.response.use(
   (response) => {
     return response;
   },
   async (error) => {
+    const { config, response } = error;
+
+    // const [test, setTest] = useRecoilState(testState);
+    // setTest(59);
+
+    count += 1;
+
+    /**
+     * 서버 오류 시 무한 재귀를 대응하기위한 if문
+     * */
+    if (count > 2) {
+      count = 0;
+      return Promise.reject(error);
+    }
+
+    await reTakeToken();
+
     if (notFoundGroupDetail(error)) {
       return (window.location.href = '/not-found');
     }
 
-    const { config, response } = error;
-    if (response?.status === 401) {
-      if (isErrorCase(response.data.status.code)) {
-        removeAccessToken();
-        window.location.href = process.env.REACT_APP_SERVICE_URL as string;
-      }
-      if (!lock) {
-        if (response?.data.status.code === 1204) {
-          lock = true;
-          await reTakeToken();
-          lock = false;
-          return;
-        }
-      }
+    if (response?.data.status.code === 1204) {
+      await reTakeToken();
+
+      return api(config);
     }
+
     return Promise.reject(error);
   },
 );
