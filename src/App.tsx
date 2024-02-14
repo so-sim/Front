47ce8038444a98b68912cf595a09b395f8dba251
Jong1co <narira0922@gmail.com>
2024-02-14 23:38:08 +0900

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Router from './routes/Router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
 import Toast, { ToastPopUp } from './components/@common/Toast';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AxiosError } from 'axios';
 import { TOAST_ERROR } from './constants/Toast';
 import { GlobalConfirmModal } from './components/@common/Modal/ConfirmModal';
