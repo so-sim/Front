372c6f0d4ebf8bea1d57f24537378f683156d6f9
Jong1co <narira0922@gmail.com>
2024-03-30 08:30:44 +0900

    fix: participantList 오류 수정

--- src/queries/Group/useParticipantList.ts
+++ src/queries/Group/useParticipantList.ts
@@ -2,5 +2,7 @@ import { getParticipantList } from '@/api/Group';
 import { useQuery } from '@tanstack/react-query';
 
 export const useParticipantList = (groupId: number | undefined) => {
-  return useQuery(['participantList', groupId], () => getParticipantList(groupId));
+  return useQuery(['participantList', groupId], () => getParticipantList(groupId), {
+    enabled: !!groupId,
+  });
 };
